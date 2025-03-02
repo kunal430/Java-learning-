f252fa1b56036e0960a4e5c5669d821d6904d4c9
kunal430 <kunalofficial5188@gmail.com>
2025-03-02 23:17:12 +0530

    add veriable operation code

---
+++ BASE/veriables.java
@@ -0,0 +1,8 @@
+class veriables{
+    public static void main(String args[]){
+        int a = 54 ;
+        int b = 100 ;
+        int sum = a + b ;
+        System.out.println("sum of a + b is -"+ sum);
+    }
+}
