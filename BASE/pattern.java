7f1075b1950c0d42897159f3cb00b40d300b01c8
kunal430 <kunalofficial5188@gmail.com>
2025-03-02 22:05:18 +0530

    add PRINT A PATTERN code

---
+++ BASE/pattern.java
@@ -0,0 +1,14 @@
+// print a pattrnt 
+// @
+// @@
+// @@@
+// @@@@
+
+class pattern{
+    public static void main(String args[]){
+        System.out.println(" @");
+        System.out.println(" @ @");
+        System.out.println(" @ @ @");
+        System.out.println(" @ @ @ @");
+    }
+}
