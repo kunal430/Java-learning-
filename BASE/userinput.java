3541b6458e77d65c4062665783af233fee17d352
kunal430 <kunalofficial5188@gmail.com>
2025-03-06 17:47:01 +0530

    minor changes

--- BASE/userinput.java
+++ BASE/userinput.java
@@ -1,9 +1,9 @@
-import java.util.Scanner;
+import java.util.*;
 public class userinput{
     public static void main(String args[]){
         Scanner sc  = new Scanner(System.in);
         String name = sc.nextLine();
-        System.out.println(name);
+        System.out.println("you enterd - "+name);
         sc.close();
     }    
 }
