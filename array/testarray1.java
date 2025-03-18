0f1a0d71c6c7f5108c8c1d3ccd4ba3ae37326b7c
kunal430 <kunalofficial5188@gmail.com>
2025-03-18 23:16:06 +0530

    add practice code file

---
+++ array/testarray1.java
@@ -0,0 +1,17 @@
+public class testarray1 {
+    public static void main(String args[]){
+        int anum[]= new int[5]; // declaring am array
+        anum[0]=20;
+        anum[1]=30;
+        anum[2]=40;
+        anum[3]=50;
+        anum[4]=60;
+        // printing all arrya 
+        System.out.println("array at 0th position-"+anum[0]);
+        System.out.println("array at 1st position-"+anum[1]);
+        System.out.println("array at 2nd position-"+anum[2]);
+        System.out.println("array at 3rd position-"+anum[3]);
+        System.out.println("array at 4th position-"+anum[4]);
+        
+    }
+}
