c8abb5307842f7e67ff178ec99fd226a44e703b7
kunal430 <kunalofficial5188@gmail.com>
2025-03-18 19:53:43 +0530

    add practice code

---
+++ array/simplearray/testarray1.java
@@ -0,0 +1,15 @@
+public class testarray {
+
+    public static void main(String[] args) {
+        int anum[] = new int[4];
+        anum[0]=50;
+        anum[1]=100;
+        anum[2]=200;
+        anum[3]=300;
+        System.out.println("array at 0th position - "+anum[0]);
+        System.out.println("array at 1st position - "+anum[1]);
+        System.out.println("array at 2nd position - "+anum[2]);
+        System.out.println("array at 3rd position - "+anum[3]);
+        System.out.println("array at 4th position - "+anum[4]);
+    }
+}
