23bafa28d1727ec27297c85db2e0d8c92949c14e
kunal430 <kunalofficial5188@gmail.com>
2025-03-18 23:59:33 +0530

    add practice code file

---
+++ array/testarray2.java
@@ -0,0 +1,13 @@
+public class testarray2 {
+    public static void main(String args[]){
+
+        // array of string data type
+        String cart[]= new String[2]; 
+        cart[0]="shampo";
+        cart[1]="conditioner";
+        System.out.println("string array\n");
+        System.out.println("array at 0th position -"+cart[0]);
+        System.out.println("array at 1st position -"+cart[1]);
+    }
+    
+}
