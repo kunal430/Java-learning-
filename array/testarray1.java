69116e4b75c07c3aba4a305067b2cb2524b5398e
kunal430 <kunalofficial5188@gmail.com>
2025-03-19 09:33:17 +0530

    modify a comment

--- array/testarray1.java
+++ array/testarray1.java
@@ -6,7 +6,7 @@ public class testarray1 {
         anum[2]=40;
         anum[3]=50;
         anum[4]=60;
-        // printing all arrya 
+        // printing all array 
         System.out.println("array at 0th position-"+anum[0]);
         System.out.println("array at 1st position-"+anum[1]);
         System.out.println("array at 2nd position-"+anum[2]);
