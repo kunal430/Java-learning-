536fd9f6431b61b4e27f729125598d419fa71bc8
kunal430 <kunalofficial5188@gmail.com>
2025-03-19 09:44:38 +0530

    add practice code file

---
+++ array/testarray3.java
@@ -0,0 +1,14 @@
+public class testarray3{
+    public static void main(String args[]){
+        // array of char data type
+       char spell[] = {'k','u','n','a','l'};
+
+       System.out.println("array at 0th position - "+spell[0]);
+       System.out.println("array at 1st position - "+spell[1]);
+       System.out.println("array at 2nd position - "+spell[2]);
+       System.out.println("array at 3rd position - "+spell[3]);
+       System.out.println("array at 4th position - "+spell[4]);
+
+
+    }
+}
