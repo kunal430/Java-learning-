973959ed99dc5d285da9b23ba6627b8e890d0d2f
kunal430 <kunalofficial5188@gmail.com>
2025-03-19 10:17:28 +0530

    add practice code file

---
+++ array/testarray4.java
@@ -0,0 +1,12 @@
+public class testarray4 {
+    public static void main(String[] args) {
+        // array of double data type
+       double points[]={10.5,20.5};
+       // printing all array with index no.
+       System.out.println(" double(data type) array\n");
+       System.out.println("array at 0th position - "+points[0]);
+       System.out.println("array at 1st position - "+points[1]);
+
+    }
+    
+}
