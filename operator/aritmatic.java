97386136ac54ae3fcde38b67cc43111b59b2fc85
kunal430 <kunalofficial5188@gmail.com>
2025-03-20 13:28:44 +0530

    add practice code file

---
+++ operator/aritmatic.java
@@ -0,0 +1,16 @@
+package operator;
+
+public class aritmatic {
+    public static void main(String args[]){
+        int a = 10; // initializing values
+        int b = 20;
+
+        System.out.println("aritmatic operator \n");
+        System.out.println("a = "+a);
+        System.out.println("b = "+b);
+        System.out.println("A+B = "+(a+b));
+        System.out.println("A-B = "+(a-b));
+        System.out.println("A*B = "+(a*b));
+        System.out.println("A/B = "+(a/b));
+    } 
+}
