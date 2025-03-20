d5b9a5ef6940c7ade4a0dc6e994a06b92b386263
kunal430 <kunalofficial5188@gmail.com>
2025-03-20 13:48:24 +0530

    minor changes

--- operator/relational.java
+++ operator/relational.java
@@ -5,6 +5,9 @@ public class relational {
         int a=10; // assigning the value 
         int b=20;
 
+        // arithmatic opetrations using declared veriables
+        // output of the relational operator will allways in boolean datatype(true  and false)
+
         System.out.println("relatinal operatpr\n");
         System.out.println("a==b -"+(a==b));
         System.out.println("a!=b -"+(a!=b));
