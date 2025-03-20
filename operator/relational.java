3a53577079af0e0106db500f5f7e122752251db9
kunal430 <kunalofficial5188@gmail.com>
2025-03-20 13:30:04 +0530

    add practice code file

---
+++ operator/relational.java
@@ -0,0 +1,17 @@
+package operator;
+
+public class relational {
+    public static void main(String[] args) {
+        int a=10; // assigning the value 
+        int b=20;
+
+        System.out.println("relatinal operatpr\n");
+        System.out.println("a==b -"+(a==b));
+        System.out.println("a!=b -"+(a!=b));
+        System.out.println("a>b -"+(a>b));
+        System.out.println("a<b -"+(a<b));
+        System.out.println("a>=b -"+(a>=b));
+        System.out.println("a<=b -"+(a<=b));
+
+    }
+}
