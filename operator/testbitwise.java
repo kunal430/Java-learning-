8fb39aca3bd3ba37f09d3649b60ab0c0df7ab346
kunal430 <kunalofficial5188@gmail.com>
2025-03-21 10:29:11 +0530

    add code file

---
+++ operator/testbitwise.java
@@ -0,0 +1,13 @@
+package operator;
+
+public class testbitwise{
+    public static void main(String[] args) {
+        int a= 30;
+        int b= 10;
+    
+        System.out.println("bitwise operator\n");
+        System.out.println("a & b -"+(a&b));
+        System.out.println("a | b -"+(a|b));
+
+    }
+}
