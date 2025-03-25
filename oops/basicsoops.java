655f85b9a7ba4b241d54f97c6c6c478f7f98bfeb
kunal430 <kunalofficial5188@gmail.com>
2025-03-25 17:02:20 +0530

    add code file

---
+++ oops/basicsoops.java
@@ -0,0 +1,27 @@
+package oops;
+
+class Student{
+    String name ;
+    String age ;
+    public void atendence(){
+        System.out.println("atended");
+        System.out.println(this.name);
+        System.out.println(this.age);
+    } 
+}
+
+
+
+public class basicsoops {
+    public static void main(String[] args) {
+        
+        Student s1 = new Student();
+        s1.name="kunal";
+        s1.age="24";
+        s1.atendence();
+    }
+   
+
+
+    
+}
