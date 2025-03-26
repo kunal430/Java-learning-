d56c43db346463825108493901b8823d0faef5cd
kunal430 <kunalofficial5188@gmail.com>
2025-03-26 19:25:58 +0530

    minor changes

--- operator/testbitwise.java
+++ operator/testbitwise.java
@@ -2,12 +2,16 @@ package operator;
 
 public class testbitwise{
     public static void main(String[] args) {
-        int a= 30;
+        int a= 30; // declaring values
         int b= 10;
-    
+        int c= 0;
         System.out.println("bitwise operator\n");
-        System.out.println("a & b -"+(a&b));
-        System.out.println("a | b -"+(a|b));
+
+        c= a & b ;
+        System.out.println("a & b -" +c);
+
+        c=a|b;
+        System.out.println("a | b -"+c);
 
     }
 }
