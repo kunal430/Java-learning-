5494aa44989032dd9f2ffe9411a0e49684491457
kunal430 <kunalofficial5188@gmail.com>
2025-04-15 16:32:44 +0530

    add file

---
+++ conditional staetment/ava_nested_if_example.java
@@ -0,0 +1,12 @@
+package conditional staetment;
+
+public class ava_nested_if_example{
+    int age = 29;
+    int waight = 68;
+
+    if (age>18){
+        if(waight>50){
+            System.out.println("you are eligible to donet blood");
+        }
+    }
+}
