c53b8668f9464f1c9fb64a4b7c8e693cd099c237
kunal430 <kunalofficial5188@gmail.com>
2025-04-15 16:30:45 +0530

    initial commit

---
+++ conditional staetment/if_statement_example.java
@@ -0,0 +1,10 @@
+package conditional staetment;
+
+public class if_statement_example {
+    public static void main(String[] args) {
+        int age = 19;
+        if (age>18){
+            System.out.println("you are eligible");
+        }
+    }
+}
