d84486f5d0708168358870e78273d0b520667195
kunal430 <kunalofficial5188@gmail.com>
2025-04-15 16:31:55 +0530

    initial commit

---
+++ conditional staetment/if_else_example.java
@@ -0,0 +1,11 @@
+package conditional staetment;
+
+ class if_else_example {
+    int age = 12;
+    if (age > 18){
+        System.out.println("you are eligible for voting");  
+    }else{
+        System.out.println("you are not eligible for vote");
+    }
+    
+}
