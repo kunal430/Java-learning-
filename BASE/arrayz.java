96bcab06d1d1637c21d10d64f4f1c06e3a6d1866
kunal430 <kunalofficial5188@gmail.com>
2026-02-14 00:18:31 +0530

    array learning

---
+++ BASE/arrayz.java
@@ -0,0 +1,15 @@
+public class arrayz {
+    public static void main(String[] args) {
+        int[] a = {20 , 30};
+        int[][] b ={
+            {1,2},
+            {3,4}
+        };
+
+        for (int i = 0 ; i < a.length ;i++){
+            System.out.println(a[i]);
+        }
+
+    }
+    
+}
