8cdef8be7939bd8bc5122d05a8460dbe78e08e16
kunal430 <kunalofficial5188@gmail.com>
2026-02-14 16:51:23 +0530

    2d array learning

--- BASE/arrayz.java
+++ BASE/arrayz.java
@@ -8,5 +8,8 @@ public class arrayz {
     }
     
 }
-/* o/p  
+/* o/p 
+
+  20 30
+  
 */
---
+++ BASE/arrayz2D.java
@@ -0,0 +1,16 @@
+public class arrayz2D {
+    public static void main(String[] args) {
+        int a[][] = {  // 2d array
+            {1,2},
+            {3,4}
+        };
+
+        for(int i = 0 ; i < a.length;i ++){    // itrating 2d array
+            for (int j = 0 ; j < a[i].length;j++){
+                System.out.print(a[i][j]+ " ");
+            }
+            System.out.println();
+        }
+    }
+    
+}
