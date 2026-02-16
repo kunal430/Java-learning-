43642c10f158335ab9bc6f07db2cb67b8cb1d4c7
kunal430 <kunalofficial5188@gmail.com>
2026-02-16 22:38:54 +0530

    add comments

--- BASE/functions/sumFunc.java
+++ BASE/functions/sumFunc.java
@@ -3,18 +3,28 @@ package functions;
 import java.util.Scanner;
 
 public class sumFunc {
-    public static int addition(int a , int b){
+    public static int addition(int a , int b){ // create sum function
         int sum = a+b ;
         return sum ;
     }    
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in); // getting user input 
         System.out.println("enter a = ");
         int a = sc.nextInt();
         System.out.println("enter b = ");
         int b = sc.nextInt();
                 
-        System.out.println("sum of a and b is = "+ addition(a, b));
+        System.out.println("sum of a and b is = "+ addition(a, b)); // func to summ 2 numbers
         sc.close();
     }
 }
+ /* 
+
+o/p 
+enter a = 
+8
+enter b = 
+1
+sum of a and b is = 9
+
+  */
