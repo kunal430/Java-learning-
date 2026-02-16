7c0cfc73c270696f505d6206ef2c1b26beff73af
kunal430 <kunalofficial5188@gmail.com>
2026-02-17 00:06:30 +0530

    add comments

--- BASE/functions/productTwoNum.java
+++ BASE/functions/productTwoNum.java
@@ -3,21 +3,27 @@ package functions;
 import java.util.Scanner;
 
 public class productTwoNum {
-    public static int multiply(int a , int b){
-        return a* b ;
+    public static int multiply(int a , int b){ // create sum function
+        return a* b ; // without veriable
     }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("enter a = ");
+        System.out.println("enter a = ");  // // getting user input 
         int a = sc.nextInt();
         System.out.println("enter b = ");
         int b = sc.nextInt();
 
-        System.out.println("product of a and b = "+multiply(a,b));
+        System.out.println("product of a and b = "+multiply(a,b)); //  func to multiply 2 numbers
         sc.close();
     }
     
 }
 
-
+/* o/p 
+enter a = 
+88 
+enter b = 
+10
+product of a and b = 880
+ */
